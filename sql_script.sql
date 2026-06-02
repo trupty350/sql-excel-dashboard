21ff278d46850f91f9ba461d1074cb21c37b9943
Trupti <103814126+trupty350@users.noreply.github.com>
2026-06-02 17:42:05 +0200

    Add SQL table script

---
+++ sql_script.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sales_data (
+    OrderID INT,
+    Customer VARCHAR(50),
+    Category VARCHAR(50),
+    SalesAmount DECIMAL(10,2),
+    Country VARCHAR(50),
+    OrderDate DATE
+);
+
+SELECT * FROM sales_data;
